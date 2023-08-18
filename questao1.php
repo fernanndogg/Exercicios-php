60cff8e6b65ffa118ba06bfbf1bdbe3b2c397428
fernanndogg <105644403+fernanndogg@users.noreply.github.com>
2023-08-18 17:51:03 -0300

    Initial commit

---
+++ questao1.php
@@ -0,0 +1,9 @@
+<?php
+$idade = 20;
+while ($idade >= 1)
+{
+echo "<p>Contagem: $idade</p>";
+$idade--;
+$idade--;
+}
+?>
---
+++ questao2.php
@@ -0,0 +1,8 @@
+<?php
+$numero = 1;
+while ($numero <= 10)
+{
+echo "<p>Contagem: $numero</p>";
+$numero++;
+}
+?>
